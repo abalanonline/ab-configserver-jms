23c224577f815cd7ecb7859ba2ef714e183652c0
Aleksei Balan <abalanonline@gmail.com>
2020-02-22 11:58:26 -0500

    Edgware downgrade

--- configuration-client/src/test/java/com/example/configurationclient/ConfigurationClientApplicationTest.java
+++ configuration-client/src/test/java/com/example/configurationclient/ConfigurationClientApplicationTest.java
@@ -18,14 +18,17 @@ package com.example.configurationclient;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.cloud.context.refresh.ContextRefresher;
 import org.springframework.core.env.ConfigurableEnvironment;
-
+import org.springframework.test.context.junit4.SpringRunner;
+ 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ConfigurationClientApplicationTest {
 
--- configuration-service/src/test/java/com/example/configurationservice/ConfigurationServiceApplicationTest.java
+++ configuration-service/src/test/java/com/example/configurationservice/ConfigurationServiceApplicationTest.java
@@ -18,11 +18,14 @@ package com.example.configurationservice;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.config.server.environment.EnvironmentController;
-
+import org.springframework.test.context.junit4.SpringRunner;
+ 
+@RunWith(SpringRunner.class)
 @SpringBootTest(properties = { "spring.profiles.active=native" })
 public class ConfigurationServiceApplicationTest {
 
